e623b714671b899859b4a5961ac4b44c37fd0a3a
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-11-02 16:19:45 +0100

    Consegna esercizio base

--- Program.cs
+++ Program.cs
@@ -1,1 +1,129 @@
-﻿
+﻿/*
+ * Nel progetto csharp-oop-shop, creare la classe Prodotto che gestisce i prodotti dello shop. Un prodotto è caratterizzato da:
+    codice (numero intero)
+    nome
+    descrizione
+    prezzo
+    iva
+
+    Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che:
+    1. alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+    2. Il codice prodotto sia accessibile solo in lettura
+    3. Gli altri attributi siano accessibili sia in lettura che in scrittura
+    4. Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
+    5. Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
+
+    BONUS: create un metodo che restituisca il codice con un pad left di 0 per arrivare a 8 caratteri 
+    (ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
+*/
+
+
+// istanzia prodotto con tutte le informazioni usando il secondo costruttore
+Prodotto prodotto1 = new Prodotto("realme", "octacore, 12GB RAM, quattro fotocamere", 500.99, 22);
+// istanzia prodotto con solo il codice random usando il primo costruttore
+//Prodotto prodotto2 = new Prodotto();
+
+//accessibile solo in lettura
+//stampa il codice prodotto
+Console.WriteLine(prodotto1.GetCodice());
+
+
+//accessibili sia lettura che scrittura
+//leggere il nome
+Console.WriteLine(prodotto1.GetNome());
+//settare il nome
+//prodotto1.SetNome("samsung");
+
+//leggere il descrizione
+Console.WriteLine(prodotto1.GetDescrizione());
+//settare il nome
+//prodotto1.SetDescrizione("ciao sono la nuova descrizione tramite metodo");
+
+
+//prezzo senza iva
+Console.WriteLine(prodotto1.Prezzo());
+//prezzo ivato
+Console.WriteLine(prodotto1.PrezzoIvato());
+
+//Nome esteso
+prodotto1.NomeEsteso();
+
+//stampa intero prodotto 
+prodotto1.Stampa();
+
+
+
+public class Prodotto
+{
+    private int codice;
+    public string nome;
+    public string descrizione;
+    private double prezzo;
+    private int iva;
+
+    public Prodotto()
+    {
+        codice = new Random().Next(1, 100000000);
+    }
+
+    public Prodotto(string nome, string descrizione, double prezzo, int iva)
+    {
+        codice = new Random().Next(1, 100000000);
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+
+    public void Stampa()
+    {
+        Console.WriteLine("Codice prodotto: " + codice);
+        Console.WriteLine("Nome prodotto: " + nome);
+        Console.WriteLine("Descrizione prodotto: " + descrizione);
+        Console.WriteLine("Prezzo netto: " + prezzo + " euro");
+        Console.WriteLine("Prezzo ivato: " + PrezzoIvato() + " euro");
+        Console.WriteLine("Iva: " + iva);
+    }
+
+    //leggere il codice
+    public int GetCodice()
+    {
+        return codice;
+    }
+
+    //leggere o modificare il nome
+    public string GetNome()
+    {
+        return nome;
+    }
+    public void SetNome(string input)
+    {
+        this.nome = input;
+;   }
+    //leggere o modificare la descrizione 
+    public string GetDescrizione()
+    {
+        return descrizione;
+    }
+    public void SetDescrizione(string input)
+    {
+        this.descrizione = input;
+;   }
+    
+
+
+
+    public double Prezzo()
+    {
+        return prezzo;
+    }
+    public double PrezzoIvato()
+    {
+        return prezzo + (prezzo * 0.22);
+    }
+
+    public void NomeEsteso()
+    {
+        Console.WriteLine(codice + " " + nome);
+    }
+}
